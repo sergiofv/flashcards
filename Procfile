9c367b70777ee791ac67f895b8ca9a49a9e6a935
Sergio FV <sfvsound@gmail.com>
2020-11-27 19:50:51 +0100

    heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn prueba.wsgiapplication --log-file -
--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 't+mjfluol@vy6sqq^wbrc*9nfjqnt*53^5lm6n#3i*^2+85y4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,3 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
